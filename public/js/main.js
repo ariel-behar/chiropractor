857837325e4d0fbd3d557dd542157a60b7510242
Ariel Behar <ariel-behar@hotmail.com>
2020-04-23 19:04:06 +0300

    Opening Commit

---
+++ public/js/main.js
@@ -0,0 +1,2 @@
+"use strict";(document.onload=function(){setTimeout(function(){var t=document.getElementById("fading-in-menu");t.style.transition="2s",t.style.opacity=1},1e3)})();
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+(document.onload = (() => {
+    setTimeout(() => {
+        let menu = document.getElementById('fading-in-menu');
+        menu.style.transition = '2s';
+        menu.style.opacity = 1;
+    }, 1000);
+}))();
+
+
